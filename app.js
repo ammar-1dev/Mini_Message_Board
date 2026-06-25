3cc6eb88bb4ffecbcc3f64ee4e64d2f5cb9ecfc7
abrahamjust <162578329+abrahamjust@users.noreply.github.com>
2026-06-25 09:59:56 +0000

    changed port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/", indexRouter);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
     if (error) {
         throw new CustomStartError("Start up error");
